2b139de2dbd76c9c51b32cc390ba6785bc854616
Aaron Namba <aaron@aaronnamba.com>
2018-10-30 13:05:24 +0900

    added def_equals (compare uuids)

--- src/neo4j/base.cr
+++ src/neo4j/base.cr
@@ -17,20 +17,18 @@ module Neo4j
     end
 
     macro included
+      def_equals(@_uuid)
+
       # use leading underscore to indicate a property/ivar that should *not* be persisted to neo4j
       property _uuid : String # special because it is persisted on create, but never on update
-      property _node : Neo4j::Node
+      property _node : Neo4j::Node # snapshot of db node
       property _rel : Neo4j::Relationship?
 
-      @@label : String = "{{@type.name}}"
+      class_getter label : String = "{{@type.name}}"
       @@limit = 500 # for safety, lift as needed; FIXME once we have a query proxy system
 
       def self.connection
         Neo4j::Bolt::Connection.new(ENV["NEO4J_URL"]? || "bolt://neo4j@localhost:7687", ssl: false)
-      end
-
-      def self.label
-        @@label
       end
 
       def initialize
