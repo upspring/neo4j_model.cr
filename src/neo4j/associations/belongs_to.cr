c25bc0fdeec3198f97d21c2fece39413e58a0ea0
Aaron Namba <aaron@aaronnamba.com>
2018-12-19 13:32:56 +0900

    pluralization fixes for has_one and belong_to

--- src/neo4j/associations/belongs_to.cr
+++ src/neo4j/associations/belongs_to.cr
@@ -3,15 +3,22 @@ require "neo4j"
 module Neo4j
   module Model
     # equivalent of ActiveNode has_one :in
-    macro belongs_to(klass, *, rel_type, name = "", unique = true)
-      {% name = (name == "" ? klass.id.underscore : name.id) %}
+    macro belongs_to(klass, *, rel_type, name = "", plural = "", unique = true)
+      {%
+        name = (name == "" ? klass.id.underscore : name.id)
+        plural = if plural == ""
+                   # can handle a very extremely simple cases here
+                   (name + "s").gsub(/sss$/, "sses").gsub(/mans$/, "men").gsub(/childs$/, "children")
+                 else
+                   plural.id
+                 end
+      %}
 
       setter {{name}}_id : String?
 
       class QueryProxy
         # QueryProxy instance method, for chaining
-        # FIXME: just adding 's' to pluralize is not always right
-        def {{name}}s(assoc_obj_variable_name = :{{name}}, assoc_rel_variable_name = :r) : {{klass.id}}::QueryProxy
+        def {{plural}}(assoc_obj_variable_name = :{{name}}, assoc_rel_variable_name = :r) : {{klass.id}}::QueryProxy
           proxy = {{klass.id}}::QueryProxy.new("MATCH (#{obj_variable_name}:#{label})<-[#{assoc_rel_variable_name}:{{rel_type.id}}]-(#{assoc_obj_variable_name}:#{{{klass.id}}.label})",
                                                "RETURN #{assoc_obj_variable_name}, #{assoc_rel_variable_name}").query_as(assoc_obj_variable_name, assoc_rel_variable_name)
           self.chain proxy
@@ -19,15 +26,14 @@ module Neo4j
 
         # QueryProxy instance method, for normal use (returns object)
         def {{name}}(assoc_obj_variable_name = :{{name}}, assoc_rel_variable_name = :r) : {{klass.id}}?
-          {{name}}s(assoc_obj_variable_name, assoc_rel_variable_name).first_with_rel?
+          {{plural}}(assoc_obj_variable_name, assoc_rel_variable_name).first_with_rel?
         end
       end
 
-      # FIXME: just adding 's' to pluralize is not always right
       # instance method, to start a chained query
-      def {{name}}s(assoc_obj_variable_name = :{{name}}, assoc_rel_variable_name = :r)
+      def {{plural}}(assoc_obj_variable_name = :{{name}}, assoc_rel_variable_name = :r)
         # create a proxy for all queries related to this association
-        proxy = QueryProxy.new.{{name}}s(assoc_obj_variable_name, assoc_rel_variable_name)
+        proxy = QueryProxy.new.{{plural}}(assoc_obj_variable_name, assoc_rel_variable_name)
 
         # this is the beginning of the chain, should start with a uuid match (provided by #query_proxy)
         context = query_proxy
@@ -36,7 +42,7 @@ module Neo4j
 
       # instance method, for normal use (returns object)
       def {{name}}(assoc_obj_variable_name = :{{name}}, assoc_rel_variable_name = :r)
-        {{name}}s(assoc_obj_variable_name, assoc_rel_variable_name).first_with_rel?
+        {{plural}}(assoc_obj_variable_name, assoc_rel_variable_name).first_with_rel?
       end
 
       def {{name}}=(target : {{klass.id}}?)
